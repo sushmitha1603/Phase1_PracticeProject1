0db77dacff4050422a68616569d6df933c569da3
sushmitha <sushmitha16031999@gmail.com>
2021-09-04 13:30:09 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace student
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fpath = @"C:\Practice_project\Student.txt";
+            StreamReader sr = new StreamReader(fpath);
+            string fileContent = sr.ReadToEnd();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("*****Welcome to Rainbow School**** \n");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Displaying the student details");
+            Console.WriteLine(fileContent);       
+            sr.Close();
+        }
+    }
+}
